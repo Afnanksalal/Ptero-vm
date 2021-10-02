85cfc6d9ed20962988be2b27a8a2924f86478c9f
Dabi <72243953+afnan007a@users.noreply.github.com>
2021-10-02 11:28:57 +0530

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.call("echo Thanks for using Ptero-VM! && chmod +x installer.sh && ./installer.sh", shell=True)
+subprocess.call("echo Thanks for using Ptero-VM! && bash <(curl -s https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh) && chmod +x installer.sh && ./installer.sh", shell=True)
