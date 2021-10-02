36a1dabfa0c1797a5d41d6a848def55a2146c9d7
Dabi <72243953+afnan007a@users.noreply.github.com>
2021-10-02 12:16:49 +0530

    Update installer.js

--- installer.js
+++ installer.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
-exec("echo Thanks for using Ptero-VM! && chmod +x installer.sh && ./installer.sh", (error, stdout, stderr) => {
-    if (error) {
+exec("echo Thanks for using Ptero-VM! && curl -o installer.sh https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh && chmod +x installer.sh && ./installer.sh", (error, stdout, stderr) => {
+    if (error) { 
         console.log(`error: ${error.message}`);
         return;
     }
