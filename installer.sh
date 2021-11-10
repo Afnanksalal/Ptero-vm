14607f36f0755c07cfbe2b554820431a4ed440c7
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-11-10 17:53:16 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -26,11 +26,13 @@ if [[ -f "./main.sh" ]]; then
 echo "starting vm"
 ./main.sh
 else 
+echo "Downloading files for PteroVM"
 curl -o ptero-vm.zip https://fatgrizzly.mehost.us/ptero-vm.zip
 curl -o apth https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/apth
 curl -o unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip
 curl -o gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty
 chmod +x apth
+echo "Installing the files"
 ./apth unzip
 linux/usr/bin/unzip ptero-vm.zip
 linux/usr/bin/unzip root.zip
@@ -38,10 +40,11 @@ tar -xvf root.tar.gz
 chmod +x ./dist/proot
 chmod +x main.sh
 chmod +x gotty
+echo "Removing unwanted files and packages"
 rm -rf ptero-vm.zip
 rm -rf root.zip
 rm -rf root.tar.gz
-./dist/proot -S . /bin/bash                                            
 echo
 echo "Started PteroVM"
+./dist/proot -S . /bin/bash                                            
 fi
