c3b97e0cdf42f31f3cc7bff6b309d030274703c3
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-11-10 17:43:19 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -21,41 +21,26 @@ ${bold}${lightgreen} :           :     : :: ::    :   : :   : :  :         :    
                                                                                                                 
 ${bold}${lightgreen}========================================================================
  "
-download_vm(){
-curl -o ptero-vm.zip https://fatgrizzly.mehost.us/ptero-vm.zip;
-curl -o apth https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/apth;
-curl -o unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip;
-curl -o gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty;
-}
-install_vm(){
-    chmod +x apth
-    ./apth unzip
-    linux/usr/bin/unzip ptero-vm.zip
-    linux/usr/bin/unzip root.zip
-    tar -xvf root.tar.gz
-    chmod +x ./dist/proot
-    chmod +x gotty
-}                       
-remove_install_files(){
-    rm -rf ptero-vm.zip
-    rm -rf root.zip
-    rm -rf root.tar.gz
-}
-start_vm(){
-     ./dist/proot -S . /bin/bash
-}                                             
-echo
-echo "Downloading required files"
-download_vm
-echo "Downloaded"
-sleep 1
-echo "Installing Ptero VM"
-install_vm
-echo "Installed"
-sleep 1
-echo "Removing installation files"
-remove_install_files
-sleep 1
+ 
+if [[ -f "./main.sh" ]]; then
+echo "starting vm"
+./main.sh
+else 
+curl -o ptero-vm.zip https://fatgrizzly.mehost.us/ptero-vm.zip
+curl -o apth https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/apth
+curl -o unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip
+curl -o gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty
+chmod +x apth
+./apth unzip
+linux/usr/bin/unzip ptero-vm.zip
+linux/usr/bin/unzip root.zip
+tar -xvf root.tar.gz
+chmod +x ./dist/proot
+chmod +x main.sh
+chmod +x gotty
+rm -rf ptero-vm.zip
+rm -rf root.zip
+rm -rf root.tar.gz
+./dist/proot -S . /bin/bash                                            
 echo
 echo "Started PteroVM"
-start_vm
