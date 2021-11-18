5de429fe8f28e86a2feafec4c07299ea5d4b0c45
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-11-18 11:26:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
 import subprocess
 
 subprocess.call("echo Thanks for using Ptero-VM!", shell=True) 
-subprocess.call("bash <(curl -s https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh)", shell=True) 
+subprocess.call("bash installer.sh", shell=True) 
