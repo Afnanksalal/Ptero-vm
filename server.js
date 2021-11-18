0825e3ed13ef7aae2e1d6c39137395c0e575ae34
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-11-18 11:25:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("echo Thanks for using Ptero-VM! && bash <(curl -s https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh)", (error, stdout, stderr) => {
+exec("echo Thanks for using Ptero-VM! && bash installer.sh", (error, stdout, stderr) => {
     if (error) { 
         console.log(`error: ${error.message}`);
         return;
