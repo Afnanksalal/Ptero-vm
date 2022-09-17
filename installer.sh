265b18cdcce2e15c38e65e1113a12e488929aba8
MiracleCherryPie <christianpandapotans@gmail.com>
2022-09-18 00:39:58 +0800

    adding nvm to PteroVM

--- installer.sh
+++ installer.sh
@@ -56,7 +56,7 @@ if [[ -f "./installed" ]]; then
     runcmd
 else
     echo "Downloading files for PteroVM"
-    curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
+    curl -sSLo ngrok.tar.gz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz >/dev/null 2>err.log
     echo -ne '#                   (5%)\r'
     curl -sSLo ptero-vm.zip https://media.githubusercontent.com/media/afnan007a/PteroVM-Files/master/files/ptero-vm.zip >/dev/null 2>err.log
     echo -ne '##                  (10%)\r'
@@ -64,26 +64,34 @@ else
     echo -ne '####                (20%)\r'
     curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
     echo -ne '#####               (25%)\r'
+    curl -sSLo nvm-install.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh >/dev/null 2>err.log
+    echo -ne '######              (30%)\r'
     chmod +x unzip >/dev/null 2>err.log
     export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    ./unzip ngrok.zip >/dev/null 2>err.log
-    echo -ne '######               (30%)\r'
-    ./unzip ptero-vm.zip >/dev/null 2>err.log
+    tar -xvf ngrok.tar.gz >/dev/null 2>err.log
     echo -ne '#######              (35%)\r'
+    ./unzip ptero-vm.zip >/dev/null 2>err.log
+    echo -ne '########             (40%)\r'
     ./unzip root.zip
     tar -xf root.tar.gz >/dev/null 2>err.log
-    echo -ne '########             (40%)\r'
-    chmod +x ./dist/proot >/dev/null 2>err.log
     echo -ne '#########            (45%)\r'
-    chmod +x ngrok >/dev/null 2>err.log
+    chmod +x ./dist/proot >/dev/null 2>err.log
     echo -ne '##########           (50%)\r'
-    chmod +x gotty >/dev/null 2>err.log
+    chmod +x ngrok >/dev/null 2>err.log
     echo -ne '###########          (55%)\r'
+    chmod +x gotty >/dev/null 2>err.log
+    echo -ne '############         (60%)\r'
+    chmod +x nvm-install.sh >/dev/null 2>err.log
+    echo -ne '#############        (65%)\r'
+    touch .bashrc
+    bash nvm-install.sh >/dev/null 2>err.log
+    echo -ne '#############        (70%)\r'
     rm -rf ptero-vm.zip >/dev/null 2>err.log
     rm -rf root.zip >/dev/null 2>err.log
     rm -rf root.tar.gz >/dev/null 2>err.log
     rm -rf ngrok.zip >/dev/null 2>err.log
-    echo -ne '############         (60%)\r'
+    rm -rf nvm-install.sh >/dev/null 2>err.log
+    echo -ne '##############       (75%)\r'
 
     cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py" "clear")
 
